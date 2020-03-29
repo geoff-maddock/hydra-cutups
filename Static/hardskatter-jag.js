a2378c4def66d39e7f150e647a53136fba33ee73
Geoff Maddock <geoff.maddock@gmail.com>
2020-03-28 21:33:37 -0400

    Added hard-skatter to static.

---
+++ Static/hardskatter-jag.js
@@ -0,0 +1,3 @@
+// hardskatter-jag.js
+// geoff.maddock@gmail.com
+osc(10, 0.839, 300).color(0.016, 0.463, 0.2).diff(osc(45, 0.3, 100).color(0.957, 0.9, 0.9).rotate(0.148).pixelate(12).kaleid()).scrollX(19.016).colorama().luma().repeatX(4).repeatY(8).modulate(osc(1.821, -1.451, 566.137)).scale(2).out();
